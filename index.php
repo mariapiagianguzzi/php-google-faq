5c9c604a912bdea049fff2ba48676222cd799044
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-02-01 13:55:53 +0100

    php-google-faq

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq. 
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<?php 
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
